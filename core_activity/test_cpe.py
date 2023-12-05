5c569193c71fc443b30f8626cfa3fae4e6ea4b72
lucasfontini <lucasfacsul@hotmail.com>
2023-12-05 17:54:53 -0300

    Modify to change ping process

--- core_activity/test_cpe.py
+++ core_activity/test_cpe.py
@@ -109,7 +109,7 @@ def PingTest(ip):
      '''
     treated_ip = ip.split("/")[0]
     print(treated_ip, 'treated_ip')
-    ping = subprocess.getoutput(f"ping  {treated_ip} ")
+    ping = subprocess.getoutput(f"ping  {treated_ip} -c 4")
     print(ping)
     return ping
 
@@ -134,7 +134,7 @@ def TestService(circuito):
         if ip != 'none':
             ping = PingTest(ip)
             treated_ip = ip.split("/")[0]
-            if 'Enviados = 4, Recebidos = 4,' in ping:
+            if '4 packets transmitted, 4 received' in ping:
                 result['resultadoping'] = f'Ping to {treated_ip} UP'
 
             else:

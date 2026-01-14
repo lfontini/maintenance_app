acadecd35c86d4dc7f35134db1286f5dc8f522d7
lfontini <lfontini@ignetworks.com>
2026-01-14 10:35:58 -0300

    ajustado celery beatr

--- start_celery_beat.sh
+++ start_celery_beat.sh
@@ -3,5 +3,12 @@ set -e
 
 ./wait_for_db.sh
 
+echo "🧹 Resetando migrations do Celery Beat..."
+# Volta todas as migrations do django_celery_beat
+python3 manage.py migrate django_celery_beat zero --noinput
+
+# Aplica novamente todas as migrations do django_celery_beat
+python3 manage.py migrate django_celery_beat --noinput
+
 echo "🚀 Iniciando Celery Beat..."
 exec celery -A maintenance_django beat -l INFO
